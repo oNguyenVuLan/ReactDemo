c97e0e2723f1a6c43318ee067fe15323a0211536
Btnisme123 <vulan238@gmail.com>
2018-04-18 22:39:58 +0700

    render picture

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+import Bananas from './Bananas';
 
-AppRegistry.registerComponent('AwesomeProject', () => App);
+AppRegistry.registerComponent('AwesomeProject', () =>Bananas);
