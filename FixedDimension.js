c0b9f33f27e125b2d637180a595336fb5bbcdfde
LanNguyen <vulan238@gmail.com>
2018-04-20 11:25:13 +0700

    add style space between

--- FixedDimension.js
+++ FixedDimension.js
@@ -3,7 +3,9 @@ import { AppRegistry, View } from 'react-native';
 export default class FixedDimensionsBasics extends Component {
     render() {
       return (
-        <View>
+        <View style ={{flex:2,
+          justifyContent:'space-between',
+         flexDirection:'column'}}>
           <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
           <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
           <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
